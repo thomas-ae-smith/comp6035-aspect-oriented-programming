8d00b4ada6f2aa48918aa0c70dbfc7189a733d4e
taes1g09 <taes1g09@ecs.soton.ac.uk>
2013-04-17 04:50:29 +0100

    Part 1 complete.

---
+++ src/Parent.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+
+/**
+ * @author taes1g09
+ *
+ */
+
+@logging
+public class Parent {
+
+//	@logging
+	public class Daughter {
+		public void complain(){
+			System.out.println("Awww");
+		}
+		
+	}
+
+	Parent(){
+		Son tom = new Son();
+		Son sam = new Son();
+		Daughter libby = new Daughter();
+	}
+
+
+}
---
+++ src/Son.java
@@ -0,0 +1,7 @@
+	@logging
+	public class Son {
+		public void complain(){
+			System.out.println("Not fair");
+		}
+		public Son(){}
+	}
