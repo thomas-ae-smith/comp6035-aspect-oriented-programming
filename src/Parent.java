ea595f94c52d6fcc6d41472549a03c2263e53383
taes1g09 <taes1g09@ecs.soton.ac.uk>
2013-04-17 05:52:31 +0100

    Initial foray into using maps. About to switch to insertion.

--- src/Parent.java
+++ src/Parent.java
@@ -12,16 +12,25 @@ public class Parent {
 
 //	@logging
 	public class Daughter {
+		public String name;
+		public String talk;
 		public void complain(){
+			talk = "Awww";
 			System.out.println("Awww");
 		}
 		
+		public Daughter() {
+			name = "Beth";
+		}
 	}
 
 	Parent(){
 		Son tom = new Son();
-		Son sam = new Son();
+		Son sam = new Son("Sam");
 		Daughter libby = new Daughter();
+		libby.name = "Libby";
+		tom.complain();
+		libby.complain();
 	}
 
 
--- src/Son.java
+++ src/Son.java
@@ -1,7 +1,12 @@
 	@logging
 	public class Son {
+		public String name;
+		public String talk;
+		
 		public void complain(){
+			talk = "not fair";
 			System.out.println("Not fair");
 		}
-		public Son(){}
+		public Son(){name = "tom";}
+		public Son(String newName){name = newName;}
 	}
