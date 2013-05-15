e56fe4f5b8b7a832c445e921a9e5507242c284df
taes1g09 <thomas.ae.smith@googlemail.com>
2013-05-15 18:31:43 +0300

    Part 1 done.

---
+++ Templates/Templates/Part1.cpp
@@ -0,0 +1,29 @@
+//
+//  Part1.cpp
+//  Templates
+//
+//  Created by taes1g09 on 15/05/2013.
+//  Copyright (c) 2013 taes1g09. All rights reserved.
+//
+
+
+#include <iostream>
+#include "Part1.h"
+
+int main (int argc, const char * argv[])
+{
+    
+    //  x+(x-2)*(x-3)
+    typedef ADD< X, MUL< SUB< X, LIT<2> >, SUB< X, LIT<3> > > > EXP;
+    EXP x;
+    
+    int low = 3;
+    int high = 10;
+    
+    printf("Input %d, output %d\n", low, x.eval(low) );
+    printf("Input %d, output %d\n", high, x.eval(high) );
+    
+    
+    return 0;
+    
+}
